4e4d866e5c85e4092389fc8d3673eb12521e6748
nikhilg07 <43913706+nikhilg07@users.noreply.github.com>
2020-10-03 23:23:36 +0530

    Update helpfulmaths.cpp

--- helpfulmaths.cpp
+++ helpfulmaths.cpp
@@ -1,27 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+#define ll long long int
+#define fast ios_base::sync_with_stdio(false);cin.tie();cout.tie();
 
-int main(){
-    string str;
-    cin >> str;
-    int n = str.length();
-    int intval;
-    vector <int> v;
-    for(int i=0;i<n;i++){
-        if(str[i] == '+')
-        continue;
-        else{
-        intval = str[i]-'0';
-        v.push_back(intval);
-        }
-
+int main()
+{
+    fast
+    string s;
+    cin>>s;
+    
+    int counter1=0,counter2=0,counter3=0;
+    
+    for(int i=0;i<s.length();i+=2)
+    {
+        if(s[i]=='1') counter1++;
+        if(s[i]=='2') counter2++;
+        if(s[i]=='3') counter3++;
     }
-
-    sort(v.begin(),v.end());
-    for(int i=0;i<v.size();i++){
-        if(i==v.size()-1)
-        cout << v[i];
-        else
-        cout << v[i]<<'+';
-    }
+    
+    for(int i=0;i<2*counter1;i+=2)
+    s[i]='1';
+    
+    for(int i=2*counter1;i<2*counter1+2*counter2;i+=2)
+    s[i]='2';
+    
+    for(int i=2*counter1+2*counter2;i<s.length();i+=2)
+    s[i]='3';
+    
+    cout<<s;
 }
